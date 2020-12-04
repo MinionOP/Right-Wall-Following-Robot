b506533daafaf64222d945e3563b5b68cd6e6042
MinionOP <73415249+MinionOP@users.noreply.github.com>
2020-12-03 21:03:41 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 //Team 9: Charle Nguyen, Edward Sotelo, Josh McHenry
-//Milestone 10
 
 #include <xdc/std.h>
 #include <ti/sysbios/BIOS.h>
@@ -13,12 +12,9 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
-
-
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_ints.h"
-
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
@@ -27,110 +23,71 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/uart.h"
 #include "driverlib/pwm.h"
-
 #include "utils/uartstdio.h"
 
 #include "PIDController.h"
 #include "Driver.h"
 #include "Utilities.h"
 
-
-/*
-#define BASE_WIDTH 300
-#define PERIOD 400
-#define BASE_DUTY 75	//75% duty cycle
-*/
-
-
 #define BASE_WIDTH 340
 #define PERIOD 400
 #define BASE_DUTY 85	//85% duty cycle
-
-
 
 enum state_enum {FORWARD, RIGHT, LEFT, STOP};
 
 //Task
 void lightSensorTask(void);
 void PIDControllerTask(void);
-
-//Timer interrupt
-void TimerInt(void);
-
-
 void AcquireDataTask(void);
 void TxDataTask(void);
 void swapBuffer(void);
 
+//Timer interrupt
+void TimerInt(void);
 
+uint8_t overLine = 0; //Will be true if robot crosses over line
+char colorLine = 'b'; //Black crossline
+volatile uint32_t time = 0;
+volatile uint32_t timeForMaze = 0;
+volatile uint32_t time2 = 1000;
+uint8_t countDownStatus = 0; //Stop count down
+uint8_t dataCollectStatus = 0;
+int finishStatus = 0;
+double currentLDuty = BASE_DUTY;
+double currentLWidth = BASE_WIDTH;
 
 PIDController pidS;
 PIDController* pid = &pidS;
 uint8_t state = FORWARD;
-
 
 void main(void)
 {
 	InitPID(pid, PERIOD, BASE_WIDTH);
 	InitHardware();
 	BIOS_start();
-
 }
-
-//---------------------------------------------------------------------------------------------
-//If robot is currently attempting to turn right
-uint32_t rightState = 0;
-
-//Will be true if robot crosses over line
-uint8_t overLine = 0;
-
-//Black or white crossline
-//'w' = white and 'b' = black
-char colorLine = 'b';
-volatile uint32_t time = 0;
-volatile uint32_t timeForMaze = 0;
-volatile uint32_t time2 = 1000;
-
-//Stop count down
-uint8_t countDownStatus = 0;
-uint8_t dataCollectStatus = 0;
-
-int finishStatus = 0;
-
-double currentLDuty = BASE_DUTY;
-double currentLWidth = BASE_WIDTH;
 
 //Timer interrupt. Interrupt number: 39
 void TimerInt(void){
 	//Clear timer interrupt
 	TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
 	Swi_post(toDoHandle);
-
-
 }
 
 //software interrupt
 void toDo(void){
-
 	//increment time
 	time = time+1;
-
-	if(overLine){
-		timeForMaze++;
-	}
-
+	timeForMaze++;
 	//Make sure robot doesn't count same line twice
 	if(countDownStatus == 1 && time2 >0){
 		time2--;
 	}
-	else if(time2 == 0){
+	else if(time2 == 0){ // 
 		time2 = 1000;
 		countDownStatus = 0;
 	}
-
-
-	time = time%1000;
-
+	time = time%1000; // reset timer after 1 second
 	//Post to lightSensor
 	if(time%30 == 0 && countDownStatus == 0 && overLine < 3){
 		Semaphore_post(LightSem);
@@ -139,44 +96,32 @@ void toDo(void){
 	if(time%50 == 0 && !finishStatus){
 		//Post semaphore, pend semaphore in PIDControllerLoop
 		Semaphore_post(PIDSem);
-
 	}
 	//Post to AcquireData every 100ms
 	if(time%100 == 0 && dataCollectStatus == 1 && overLine < 3){
 		Semaphore_post(AcquireDataSem);
 	}
-
-
 }
-
-//---------------------------------------------------------------------------------------------
 
 //Task Handle: PIDHandle
 void PIDTask(void){
 	while(1){
 		//Pend semaphore
-
 		Semaphore_pend(PIDSem,BIOS_WAIT_FOREVER);
 		double rightWall = readRight();
 		double frontWall = readFront();
-
 		double Correction = PIDUpdate(pid, rightWall);
-
 		switch(state){
 			case FORWARD:{
 				wheelDuty(BASE_DUTY, Correction);
 				if(overLine == 3){
 					state = STOP;
 				}
-				else if(rightWall > 15 && frontWall > 15){
+				else if(rightWall > 15 ){ // need to turn right
 					setPIDRight(pid);
 					state = RIGHT;
 				}
-				else if(rightWall > 15 && frontWall < 15){
-					setPIDRight(pid);
-					state = RIGHT;
-				}
-				else if(rightWall <15 && frontWall < 6){
+				else if(rightWall <15 && frontWall < 6){ //  need to u turn
 					wheelDuty(BASE_DUTY, BASE_DUTY);
 					wheelDir(0,0);
 					state = LEFT;
@@ -184,12 +129,11 @@ void PIDTask(void){
 				break;
 			}
 			case RIGHT:{
-				wheelDuty(BASE_DUTY, Correction);
-				if(rightWall < 9){
+				wheelDuty(BASE_DUTY, Correction); // turn right
+				if(rightWall < 9){ // go forward
 					setPIDForward(pid);
 					state = FORWARD;
 				}
-
 				break;
 			}
 			case LEFT:{
@@ -208,28 +152,23 @@ void PIDTask(void){
 					UARTprintf("You finished the maze in %d seconds!\n", timeForMaze/1000);
 				}
 				break;
-
 			}
 		}
 	}
 }
 
-//---------------------------------------------------------------------------------------------
 void lightSensorTask(void){
 	while(1){
 		//Pend semaphore
 		Semaphore_pend(LightSem, BIOS_WAIT_FOREVER);
 		uint8_t temp;
 		temp = lightSensor(colorLine, dataCollectStatus);
-
 		//Number of times robot cross over line
 		overLine+=temp;
 
 		if(temp == 1){
-			countDownStatus = 1;
-			//UARTprintf("Number of line crossed: %d\n",overLine);
+			countDownStatus = 1; 
 		}
-
 		//Check whether the robot should start or stop collecting data
 		if(dataCollectStatus == 0 && temp == 1 || dataCollectStatus == 1 && temp == 0){
 			dataCollectStatus = 1;
@@ -244,11 +183,9 @@ void lightSensorTask(void){
 		else{
 			dataCollectStatus = 0;
 		}
-
 	}
 }
 
-//---------------------------------------------------------------------------------------------
 double Buffer1[20];
 double Buffer2[20];
 
@@ -258,8 +195,6 @@ uint8_t bufferPtr = 0;
 
 int currentSample = 0;
 int currentSample2 = 0;
-
-
 
 void AcquireDataTask(void){
 	while(1){
@@ -290,7 +225,6 @@ void TxDataTask(void){
 			//Turn on green LED
 			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x8);
 			UARTprintf("-------Transmission Start-------\n");
-
 		}
 		else{
 			//2nd line has been crossed
@@ -306,7 +240,6 @@ void TxDataTask(void){
 		for(i=0;i<currentSample2;i++){
 			sprintf(str,"%.2f",activeBuffer[i]);
 			UARTprintf("%s ",str);
-
 		}
 		UARTprintf("\n");
 		currentSample2 = 0;
@@ -328,9 +261,3 @@ void swapBuffer(void){
 		backBuffer = Buffer2;
 	}
 }
-
-
-
-
-
-
